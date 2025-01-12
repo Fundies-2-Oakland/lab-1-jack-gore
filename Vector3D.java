b5f4e7ae6ff3aeb48f5eaa64150d775284fc151a
Jack Gore <4jackgore@gmail.com>
2025-01-11 20:28:15 -0800

    completed questions 1-5

---
+++ Vector3D.java
@@ -0,0 +1,31 @@
+public class Vector3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public String toString() {
+        return ("(" + getX() + ", " + getY() + ", " + getZ() + ")");
+    }
+
+    public double getMagnitude() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+}
